da25f8e4a87f0365897180c9a459ee1e539d11d0
Amanda King <amapickell@gmail.com>
2021-05-15 13:18:00 -0400

    Created UsersController and basic new and show views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :business_id)
+  end
+end
