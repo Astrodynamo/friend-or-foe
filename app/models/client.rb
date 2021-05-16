b89105a7ecc700d244dec2746b968c38220e6008
Amanda King <amapickell@gmail.com>
2021-05-15 23:03:54 -0400

    Created client positivity_ratio and refactored client methods to account for 0 reports

--- app/models/client.rb
+++ app/models/client.rb
@@ -4,7 +4,22 @@ class Client < ApplicationRecord
 
     #client view helpers: average_rating, positivity_ratio, payment_record, times_blacklisted, times_reported
     def average_rating
-        ratings = reports.collect{ |report| report.rating }
-        average = ratings.sum / ratings.size
+        ratings = reports.collect{|report| report.rating}
+        if ratings.size == 0
+            "Not enough reports"
+        else
+            average = ratings.sum / ratings.size
+        end
+    end
+
+    def positivity_ratio
+        interactions = reports.collect{|report| report.positive}
+        if interactions.size == 0
+            "Not enough reports"
+        else
+            positives = 0
+            interactions.each {|interaction| positives += 1 if interaction}
+            ratio = (positives.to_f / interactions.count.to_f * 100.0).round(2)
+        end
     end
 end
