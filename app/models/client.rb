53ca75c74e20fa0f476f6bc9a5041c0a1f4c3e2a
Amanda King <amapickell@gmail.com>
2021-05-15 22:35:03 -0400

    Created Client method average_rating

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,8 @@ class Client < ApplicationRecord
     has_many :users, through: :reports
 
     #client view helpers: average_rating, positivity_ratio, payment_record, times_blacklisted, times_reported
+    def average_rating
+        ratings = reports.collect{ |report| report.rating }
+        average = ratings.sum / ratings.size
+    end
 end
